a6d9d7176812cb13458a019eaf0230873277b421
Bill Hall <da_nkp@live.com>
2026-08-24 10:39:13 -0400

    restore missing fn

--- src/platform_impl/ios/scene.rs
+++ src/platform_impl/ios/scene.rs
@@ -47,6 +47,17 @@ pub unsafe fn multiple_scenes_enabled() -> bool {
   (*num).as_bool()
 }
 
+unsafe fn handle_scene_window_events(scene: &UIScene, event: impl Fn() -> WindowEvent<'static>) {
+  if let Some(window_scene) = scene.downcast_ref::<UIWindowScene>() {
+    for window in window_scene.windows() {
+      app_state::handle_nonuser_event(EventWrapper::StaticEvent(Event::WindowEvent {
+        window_id: RootWindowId(window.into()),
+        event: event(),
+      }));
+    }
+  }
+}
+
 define_class!(
   #[unsafe(super(NSObject))]
   #[name = "TaoSceneDelegate"]
